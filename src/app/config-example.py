fd6023552572c31edd1fc97d5a5c6a34ca48707c
FMShelbyGT500 <eightynov@gmail.com>
2019-02-26 00:24:59 +0200

    Init project

---
+++ src/app/config-example.py
@@ -0,0 +1,9 @@
+secret_key = 'your secret key'
+
+# your db settings
+mysql = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
