829548581baab4d5daba717896c3c1ac7a08d328
FMShelbyGT500 <eightynov@gmail.com>
2019-02-26 10:07:05 +0200

    Added Pipfile

--- src/app/settings.py
+++ src/app/settings.py
@@ -74,7 +74,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = config.mysql
+DATABASES ={
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
