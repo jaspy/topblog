43b9748b755fba16390269b3570fa790d469c0f5
Vlad Novosadenko <eightynov@gmail.com>
2019-02-27 18:48:07 +0200

    Revert "Feat init project"

--- src/app/config-example.py
+++
@@ -1,9 +0,0 @@
-secret_key = 'your secret key'
-
-# your db settings
-mysql = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
--- src/blog/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase
-
-class TemporaryTest(TestCase):
-    def test_if_two_equals_two(self):
-        self.assertEqual(2, 2)
