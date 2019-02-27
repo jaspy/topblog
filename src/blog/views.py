0eaa04435023a8dde342ba22e215723048cc9a10
zhinzher <sanches2000009@gmail.com>
2019-02-27 23:12:00 +0200

    created url structure

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Posts
 
-# Create your views here.
+
+class PostListView(ListView):
+    model = Posts
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+    paginate_by = 4
