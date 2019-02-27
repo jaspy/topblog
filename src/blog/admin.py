ac8d74662e2285d58ecd1781feca997ea89b903b
FMShelbyGT500 <eightynov@gmail.com>
2019-02-27 16:23:01 +0200

    Set up admin page

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
-from .models import Posts
+from .models import Post, Comment
 
-admin.site.register(Posts)
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+    readonly_fields = ['author', 'created_at', 'message']
+
+
+class PostAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('created_at', 'updated_at')
+
+    fieldsets = (
+        ('', {
+            'fields': ('title', 'author')
+        }),
+        ('Date', {
+            'classes': ('collapse',),
+            'fields': ('created_at', 'updated_at')
+        }),
+        ('Content', {
+            'classes': ('extrapretty'),
+            'fields': ('content',)
+        }),
+    )
+
+    inlines = (CommentInline,)
+
+
+admin.site.register(Post, PostAdmin)
