2545f5622a82b0eed35524959a29af0aa3f67c1b
zhinzher <sanches2000009@gmail.com>
2019-03-02 11:20:44 +0200

    created Post Detail View

--- src/blog/models.py
+++ src/blog/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
     
 
 class Comment(models.Model):
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class PostListView(ListView):
@@ -7,3 +7,8 @@ class PostListView(ListView):
     context_object_name = 'posts'
     ordering = ['-created_at']
     paginate_by = 4
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
