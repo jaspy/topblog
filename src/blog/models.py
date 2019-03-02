76c0e9d2841ff8b79ea7fe459e723c3c0d42841f
zhinzher <sanches2000009@gmail.com>
2019-03-02 11:48:25 +0200

    created slug field in Model

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
-from PIL import Image
+from django.shortcuts import reverse
+from django.utils.text import slugify
+from time import time
 
+
+def gen_slug(s):
+    new_slug = slugify(s, allow_unicode=True)
+    return new_slug + '-' + str(int(time()))
 
 class Post(models.Model):
     """
@@ -12,6 +18,16 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True, db_index=True)
     content = models.TextField()
     author = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = gen_slug(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.title
--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,5 +3,5 @@ from .views import PostListView, PostDetailView
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<str:slug>/', PostDetailView.as_view(), name='post-detail'),
 ]
